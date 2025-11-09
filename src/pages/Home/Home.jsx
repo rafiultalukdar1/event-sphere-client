7dbf86d56d3e1928a0f6d6f1c09a951b733457f7
rafi0012 <rafiultalukdar007@gmail.com>
2025-11-09 13:18:30 +0600

    Added Banner Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h2 className='text-5xl font-bold py-10 text-center'>This is Home</h2>
-        </div>
+        <>
+            <Banner></Banner>
+        </>
     );
 };
 
